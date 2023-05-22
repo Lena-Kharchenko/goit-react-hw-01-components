f58587d27578118b5661869b74dcc22e6ac909eb
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-05-22 20:11:12 +0300

    update statistics

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import StatisticList from '../statisticList/statisticList';
+import css from '../statistics/statistics.module.css';
 
 export default function Statistics(props) {
   const { title, stats } = props;
 
   return (
-    <section class="statistics">
-      {title && <h2 class="title">{title}</h2>}
-      <ul class="stat_list">
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.stat_list}>
         {stats.map(({ label, percentage, id }) => (
           <StatisticList key={id} label={label} percentage={percentage} />
         ))}
