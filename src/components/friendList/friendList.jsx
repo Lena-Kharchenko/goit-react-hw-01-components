c3fb4cb9029106d291988952c38d5f391cc43b27
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-05-22 13:15:38 +0300

    update task2

---
+++ src/components/friendList/friendList.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import FriendListItem from '../friendListItem/friendListItem';
+
+export default function FriendList({ friends }) {
+  const friendList = friends.map(friend => (
+    <FriendListItem
+      key={friend.id}
+      avatar={friend.avatar}
+      name={friend.name}
+      isOnline={friend.isOnline}
+    />
+  ));
+  return <ul class="friend-list">{friendList}</ul>;
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
