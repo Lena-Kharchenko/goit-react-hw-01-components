79cb06b44ed8689b02c74a8435e3580d6ca6d9c0
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-05-22 13:12:58 +0300

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import Profile from '../components/profile/profile';
 import user from './data/user.json';
 
+import Statistics from '../components/statistics/statistics';
+import data from './data/data.json';
+
+import FriendList from '../components/friendList/friendList';
+import friends from './data/friends.json';
+
 export const App = () => {
   return (
     <div>
@@ -11,6 +17,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
